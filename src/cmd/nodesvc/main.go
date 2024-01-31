729e43b725378962801c3f343047abfab91a2077
indianaMitko62 <indiana.mitko@gmail.com>
2024-01-31 19:20:10 +0200

    Revert "http server and client testing"
    This reverts commit ce046d810cfd8acaed1d8162ae76d362368f70b4.

--- src/cmd/nodesvc/main.go
+++ src/cmd/nodesvc/main.go
@@ -2,25 +2,15 @@ package main
 
 import (
 	"log/slog"
+	"os"
 
-	"github.com/indianaMitko62/orchestrator/src/node"
 	"github.com/indianaMitko62/orchestrator/src/noderpc"
 )
 
 func main() {
-	var err error
-	slog.Info("before rpc")
-	go noderpc.Listen()
-	// if err != nil {
-	// 	slog.Error("could not start listener", "err", err)
-	// 	os.Exit(1)
-	// }
-	slog.Info("after rpc")
-	nsvc, err := node.NewNodeService()
+	err := noderpc.Listen()
 	if err != nil {
-		slog.Error("could not create Node service")
+		slog.Error("could not start listener", "err", err)
+		os.Exit(1)
 	}
-	nsvc.Node()
-
-	// add InfrastructureState
 }
