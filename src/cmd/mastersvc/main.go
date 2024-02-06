c72e49cb0abde1ea78bcc53079504f9f96655bb1
indianaMitko62 <indiana.mitko@gmail.com>
2024-02-06 15:49:09 +0200

    corresponding master changes for testing

--- src/cmd/mastersvc/main.go
+++ src/cmd/mastersvc/main.go
@@ -5,6 +5,9 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/go-connections/nat"
 	"github.com/indianaMitko62/orchestrator/src/cluster"
 	"github.com/indianaMitko62/orchestrator/src/master"
 )
@@ -20,30 +23,34 @@ func main() {
 			Name:    "Node1",
 			Address: "127.0.0.1",
 		},
-		Containers: map[string]*cluster.ContainerConfig{
-			"Container1": {
-				Name:       "Container1",
-				Image:      "hello-world",
-				Privileged: true,
-				NetworkConfig: map[string]*cluster.ContainerNetworkConfig{
-					"bridge": {
-						NetworkID:   "bridge",
-						IPv4Address: "172.16.0.2",
+		NodeState: cluster.NodeState{
+			Containers: map[string]*cluster.OrchContainer{
+				"Container1": {
+					ContainerConfig: &container.Config{
+						Hostname:     "Container1",
+						Image:        "nginx:latest",
+						ExposedPorts: map[nat.Port]struct{}{"80/tcp": {}},
 					},
+					HostConfig: &container.HostConfig{
+						PortBindings: nat.PortMap{
+							"80/tcp": []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: "8080"}},
+						},
+					},
+					NetworkingConfig: &network.NetworkingConfig{},
 				},
 			},
-		},
-		Networks: map[string]*cluster.NetworkConfig{
-			"net1": {
-				NetworkID: "net ID 1",
+			Networks: map[string]*cluster.OrchNetwork{
+				"net1": {
+					ID: "net ID 1",
+				},
 			},
-		},
-		Volumes: map[string]*cluster.VolumeConfig{
-			"vol1": {
-				VolumeID: "vol ID 1",
+			Volumes: map[string]*cluster.OrchVolume{
+				"vol1": {
+					Name: "vol1",
+				},
 			},
+			Images: map[string]*cluster.OrchImage{},
 		},
-		Images: map[string]*cluster.ImageConfig{},
 		Client: nil,
 	}
 	nodeManager1 := &cluster.NodeManager{
@@ -51,34 +58,40 @@ func main() {
 			Name:    "Node2",
 			Address: "127.0.0.1",
 		},
-		Containers: map[string]*cluster.ContainerConfig{
-			"Container2": {
-				Name:  "Container2",
-				Image: "nginx:lastest",
-				NetworkConfig: map[string]*cluster.ContainerNetworkConfig{
-					"bridge": {
-						NetworkID:   "bridge",
-						IPv4Address: "172.16.0.2",
+		NodeState: cluster.NodeState{
+			Containers: map[string]*cluster.OrchContainer{
+				"Container2": {
+					ContainerConfig: &container.Config{
+						Image:        "nginx:latest",
+						Hostname:     "Container2",
+						ExposedPorts: map[nat.Port]struct{}{"80/tcp": {}},
 					},
+					HostConfig: &container.HostConfig{
+						PortBindings: nat.PortMap{
+							"80/tcp": []nat.PortBinding{{HostIP: "0.0.0.0", HostPort: "8080"}},
+						},
+					},
+					NetworkingConfig: &network.NetworkingConfig{},
 				},
 			},
-		},
-		Networks: map[string]*cluster.NetworkConfig{
-			"net2": {
-				NetworkID: "vol ID 2",
+			Networks: map[string]*cluster.OrchNetwork{
+				"net2": {
+					ID: "net ID 2",
+				},
 			},
-		},
-		Volumes: map[string]*cluster.VolumeConfig{
-			"vol2": {
-				VolumeID: "vol ID 2",
+			Volumes: map[string]*cluster.OrchVolume{
+				"vol2": {
+					Name: "vol2",
+				},
 			},
+			Images: map[string]*cluster.OrchImage{},
 		},
-		Images: map[string]*cluster.ImageConfig{},
 		Client: nil,
 	}
+
 	clusterState.Nodes[nodeManager.Name] = nodeManager
 	clusterState.Nodes[nodeManager1.Name] = nodeManager1
-	clusterState.CollectImages()
+	clusterState.CollectImages() // to be developed and added to master logic
 	yamlData, _ := clusterState.ToYaml()
 	fmt.Printf("%s", yamlData)
 
